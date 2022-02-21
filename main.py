3b844a6baed9938c84245b4d4e3fa31a51aa4bb9
sametgundogan <sametgundogan94@icloud.com>
2022-02-21 16:15:16 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+name = input("Write your name: ").title()
+password = "W@12"
+if name == "Samet":
+    print(f"Hello {name}, your password is: ", password)
+else:
+    print(f"Hello {name}! See you later...")
